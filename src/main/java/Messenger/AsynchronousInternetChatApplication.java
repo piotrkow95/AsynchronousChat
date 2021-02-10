a65acb71b5a410d7331748c00f505a6af528b9fe
piotrkow95 <piotrkowalik246@gmail.com>
2021-02-10 11:59:38 +0100

    First commit - project setup

---
+++ src/main/java/Messenger/AsynchronousInternetChatApplication.java
@@ -0,0 +1,13 @@
+package Messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousInternetChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchronousInternetChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/Messenger/AsynchronousInternetChatApplicationTests.java
@@ -0,0 +1,13 @@
+package Messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousInternetChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
