5617171205d2dd242735aefc4cc4477f579f6b19
piotrkow95 <piotrkowalik246@gmail.com>
2021-02-24 16:25:32 +0100

    Websockets and JS for async in browser

---
+++ src/main/java/Messenger/frontend/MessageController.java
@@ -0,0 +1,27 @@
+package Messenger.frontend;
+
+
+import Messenger.frontend.dto.SendMessageDto;
+import Messenger.model.Message;
+import Messenger.services.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @MessageMapping("/publishMessage")
+    @SendTo("/topic/allMessages")
+    public Message publishMessage(SendMessageDto messageDto) throws Exception {
+        messageService.postPublicMessage(messageDto);
+        List<Message> messages = messageService.readAllPublicMessages();
+        return messages.get(messages.size()-1);
+    }
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,38 @@
+var stompClient = null;
+
+function connect() {
+    var socket = new SockJS('/websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/allMessages', function (msg) {
+            showNewMessage(JSON.parse(msg.body));
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+    stompClient.send("/app/publishMessage", {}, JSON.stringify({'text': $("#newMessageInput").val()}));
+}
+
+function showNewMessage(message) {
+    $("#allMessagesDiv")
+        .append(message.humanReadableTimestamp + " " + message.user.name + " " + message.text);
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    connect();
+    // $( "#connect" ).click(function() { connect(); });
+    // $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#sendMessage" ).click(function() { sendMessage(); });
+});
