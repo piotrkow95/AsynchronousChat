e8ca062d78522cedc228d2f77a0f9e2395eee055
piotrkow95 <piotrkowalik246@gmail.com>
2021-02-26 17:18:26 +0100

    Facebook login

--- src/main/java/Messenger/config/SecurityConfig.java
+++ src/main/java/Messenger/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package Messenger.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 
 @Configuration
 public class SecurityConfig
